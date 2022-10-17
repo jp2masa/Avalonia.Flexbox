a3731035e07048f75a2758ad012e01f25b38cc54
José Pedro <jpedrolmail@gmail.com>
2022-10-17 22:47:49 +0100

    Upgraded Avalonia to version 11.0.0-preview2.

--- samples/Avalonia.Flexbox.Demo/Converters/NumberToThicknessConverter.cs
+++ samples/Avalonia.Flexbox.Demo/Converters/NumberToThicknessConverter.cs
@@ -7,7 +7,7 @@ namespace Avalonia.Flexbox.Demo.Converters
 {
     internal sealed class NumberToThicknessConverter : IValueConverter
     {
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
         {
             if (value is int x && targetType.IsAssignableFrom(typeof(Thickness)))
             {
@@ -18,7 +18,7 @@ namespace Avalonia.Flexbox.Demo.Converters
             throw new NotSupportedException();
         }
 
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) =>
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture) =>
             throw new NotSupportedException();
     }
 }
